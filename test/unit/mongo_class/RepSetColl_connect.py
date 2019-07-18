fc7534d4774c38ac161876db70b2d2b49ddd516d
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-07-18 08:45:05 +0000

    Add RepSetColl_connect.py file

---
+++ test/unit/mongo_class/RepSetColl_connect.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  RepSetColl_connect.py
+
+    Description:  Unit testing of RepSetColl.connect in mongo_class.py.
+
+    Usage:
+        test/unit/mongo_class/RepSetColl_connect.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+
+# Local
+sys.path.append(os.getcwd())
+import mongo_class
+import version
+
+__version__ = version.__version__
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:
+
+    Methods:
+        setUp -> Initialize testing environment.
+        test_no_conn_list2 -> Test no conn_list passed, set by repset_hosts.
+        test_no_conn_list -> Test with no conn_list passed.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.name = "Mongo_Server"
+        self.user = "mongo_user"
+        self.passwd = "mongo_pwd"
+        self.host = "host_server"
+        self.port = 27017
+        self.db = "test"
+        self.coll = None
+        self.db_auth = None
+        self.repset = "mongo_repset"
+
+    def test_no_conn_list2(self):
+
+        """Function:  test_no_conn_list2
+
+        Description:  Test no conn_list passed, set by repset_hosts.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.RepSetColl(self.name, self.user, self.passwd,
+                                       self.host, self.port,
+                                       repset=self.repset)
+        mongo.conn = True
+        mongo.repset_hosts = self.host + ":" + str(self.port)
+
+        mongo.connect()
+        self.assertEqual(mongo.conn_list, self.host + ":" + str(self.port))
+
+    def test_no_conn_list(self):
+
+        """Function:  test_no_conn_list
+
+        Description:  Test with no conn_list passed.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.RepSetColl(self.name, self.user, self.passwd,
+                                       self.host, self.port,
+                                       repset=self.repset)
+        mongo.conn = True
+
+        mongo.connect()
+        self.assertEqual(mongo.conn_list, self.host + ":" + str(self.port))
+
+
+if __name__ == "__main__":
+    unittest.main()
