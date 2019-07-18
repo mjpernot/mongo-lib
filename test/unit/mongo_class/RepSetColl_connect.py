e27695d8ad65b5240a34c8445d9d5efd2d390208
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-07-18 20:00:50 +0100

    Updated test/unit/mongo_class/RepSetColl_connect.py

--- test/unit/mongo_class/RepSetColl_connect.py
+++ test/unit/mongo_class/RepSetColl_connect.py
@@ -24,6 +24,7 @@ else:
     import unittest
 
 # Third-party
+import mock
 
 # Local
 sys.path.append(os.getcwd())
@@ -45,6 +46,7 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_no_auth -> Test with auth set to false.
         test_no_conn_list2 -> Test no conn_list passed, set by repset_hosts.
         test_no_conn_list -> Test with no conn_list passed.
 
@@ -66,10 +68,29 @@ class UnitTest(unittest.TestCase):
         self.host = "host_server"
         self.port = 27017
         self.db = "test"
-        self.coll = None
-        self.db_auth = None
+        self.coll = "coll_name"
+        self.db_auth = "db_name"
         self.repset = "mongo_repset"
         self.repset_hosts = "host1:27017, host2:27107"
+
+    @mock.patch("mongo_class.pymongo.MongoClient")
+    def test_no_auth(self, mock_mongo):
+
+        """Function:  test_no_auth
+
+        Description:  Test with auth set to false.
+
+        Arguments:
+
+        """
+
+        mock_mongo.return_value = {}
+        mongo = mongo_class.RepSetColl(self.name, self.user, self.passwd,
+                                       self.host, self.port,
+                                       repset=self.repset)
+        mongo.auth = False
+
+        self.assertFalse(mongo.connect())
 
     def test_no_conn_list2(self):
 
