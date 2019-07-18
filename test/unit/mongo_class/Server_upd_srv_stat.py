70514d369fa71d6ba4f4078e96920c99dbe0300e
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-07-18 11:11:30 +0000

    Add Server_upd_srv_stat.py file

---
+++ test/unit/mongo_class/Server_upd_srv_stat.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  Server_upd_srv_stat.py
+
+    Description:  Unit testing of Server.upd_srv_stat in mongo_class.py.
+
+    Usage:
+        test/unit/mongo_class/Server_upd_srv_stat.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+
+# Local
+sys.path.append(os.getcwd())
+import mongo_class
+import version
+
+__version__ = version.__version__
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:
+
+    Methods:
+        setUp -> Initialize testing environment.
+        test_remote_host -> Test being on remote host.
+        test_local_host -> Test being on local host.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.name = "Mongo_Server"
+        self.user = "mongo_user"
+        self.passwd = "mongo_pwd"
+        self.host = "host_server"
+        self.host2 = "127.0.0.1"
+        self.port = 27017
+        self.db = "test"
+        self.coll = None
+        self.db_auth = None
+        self.data = {"uptime": 10, "connections: {"current": 1,
+                                                  "available": 9},
+                     "mem": {"resident": 1000000}}
+
+    @mock.patch("mongo_class.Server.adm_cmd")
+    def test_remote_host(self, mock_cmd):
+
+        """Function:  test_remote_host
+
+        Description:  Test being on remote host.
+
+        Arguments:
+
+        """
+
+        mock_cmd.return_value = self.data
+        mongo = mongo_class.Server(self.name, self.user, self.passwd,
+                                   self.host, self.port)
+
+        self.assertEqual((mongo.name, mongo.user, mongo.passwd, mongo.host,
+                          mongo.port, mongo.db, mongo.coll),
+                         (self.name, self.user, self.passwd, self.host,
+                          self.port, self.db, self.coll))
+
+    @mock.patch("mongo_class.Server.adm_cmd")
+    def test_local_host(self, mock_cmd):
+
+        """Function:  test_local_host
+
+        Description:  Test being on local host.
+
+        Arguments:
+
+        """
+
+        mock_cmd.return_value = self.data
+        mongo = mongo_class.Server(self.name, self.user, self.passwd,
+                                   self.host2, self.port)
+
+        self.assertEqual((mongo.name, mongo.user, mongo.passwd, mongo.host,
+                          mongo.port, mongo.db, mongo.coll),
+                         (self.name, self.user, self.passwd, self.host2,
+                          self.port, self.db, self.coll))
+
+
+if __name__ == "__main__":
+    unittest.main()
