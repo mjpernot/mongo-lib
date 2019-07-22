5a8c035577b62940d247ecd14444142790fbbf58
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-07-22 16:33:00 +0100

    Updated unit test runs

--- test/unit/mongo_libs/unit_test_run.sh
+++ test/unit/mongo_libs/unit_test_run.sh
@@ -10,4 +10,5 @@ test/unit/mongo_libs/create_instance.py
 test/unit/mongo_libs/create_slv_array.py
 test/unit/mongo_libs/crt_base_cmd.py
 test/unit/mongo_libs/crt_coll_inst.py
+test/unit/mongo_libs/ins_doc.py
 
