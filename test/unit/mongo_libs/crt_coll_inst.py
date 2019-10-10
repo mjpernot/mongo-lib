b688efcc3138661fc6fd3eaedd30d5e678451837
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-10-10 11:43:00 +0000

    Update crt_coll_inst.py

--- test/unit/mongo_libs/crt_coll_inst.py
+++ test/unit/mongo_libs/crt_coll_inst.py
@@ -40,10 +40,6 @@ class Mongo(object):
 
     Description:  Class stub holder for Mongo class.
 
-    Super-Class:
-
-    Sub-Classes:
-
     Methods:
         __init__ -> Class initialization.
 
@@ -73,10 +69,6 @@ class Cfg(object):
     """Class:  Cfg
 
     Description:  Class stub holder for Cfg class.
-
-    Super-Class:
-
-    Sub-Classes:
 
     Methods:
         __init__ -> Class initialization.
@@ -110,10 +102,6 @@ class UnitTest(unittest.TestCase):
     """Class:  UnitTest
 
     Description:  Class which is a representation of a unit testing.
-
-    Super-Class:  unittest.TestCase
-
-    Sub-Classes:
 
     Methods:
         setUp -> Initialize testing environment.
