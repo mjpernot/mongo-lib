e277e45642c2fbc0d6f6f100020a6f0f0f809cc6
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-08-17 15:44:51 +0100

    Updated test/unit/mongo_class/db_init.py v4.0.0

--- test/unit/mongo_class/db_init.py
+++ test/unit/mongo_class/db_init.py
@@ -41,7 +41,8 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
-        test_default -> Test with minimum number of arguments.
+        test_auth_uri -> Test with auth and uri present.
+        test_no_auth -> Test with no auth present.
 
     """
 
@@ -57,29 +58,45 @@ class UnitTest(unittest.TestCase):
 
         self.name = "Mongo_Server"
         self.user = "mongo_user"
-        self.passwd = "mongo_pwd"
+        self.japwd = "mongo_pwd"
         self.host = "host_server"
         self.port = 27017
         self.dbs = "test"
         self.db_auth = None
 
-    def test_default(self):
+    def test_auth_uri(self):
 
-        """Function:  test_default
+        """Function:  test_auth_uri
 
-        Description:  Test __init__ method with default arguments.
+        Description:  Test with auth and uri present.
 
         Arguments:
 
         """
 
-        mongo = mongo_class.DB(self.name, self.user, self.passwd,
+        mongo = mongo_class.DB(self.name, self.user, self.japwd,
                                self.host, self.port)
 
-        self.assertEqual((mongo.name, mongo.user, mongo.passwd, mongo.host,
-                          mongo.port),
-                         (self.name, self.user, self.passwd, self.host,
-                          self.port))
+        self.assertEqual(
+            (mongo.name, mongo.user, mongo.japwd, mongo.host, mongo.port),
+            (self.name, self.user, self.japwd, self.host, self.port))
+
+    def test_no_auth(self):
+
+        """Function:  test_no_auth
+
+        Description:  Test with no auth present.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.DB(self.name, self.user, self.japwd,
+                               self.host, self.port)
+
+        self.assertEqual(
+            (mongo.name, mongo.user, mongo.japwd, mongo.host, mongo.port),
+            (self.name, self.user, self.japwd, self.host, self.port))
 
 
 if __name__ == "__main__":
