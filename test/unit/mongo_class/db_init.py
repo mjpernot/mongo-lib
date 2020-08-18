406f48ab5de0a44176645305cca605370b79aaa1
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-08-18 14:50:32 +0100

    Updated test/unit/mongo_class/db_init.py v4.0.0

--- test/unit/mongo_class/db_init.py
+++ test/unit/mongo_class/db_init.py
@@ -41,6 +41,10 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_conn_list_attr -> Test setting the conn_list attribute.
+        test_config_attr -> Test setting the config attribute.
+        test_using_auth_db -> Test using the auth_db attribute.
+        test_using_arg -> Test with auth and arg present.
         test_auth_uri -> Test with auth and uri present.
         test_no_auth -> Test with no auth present.
 
@@ -56,6 +60,8 @@ class UnitTest(unittest.TestCase):
 
         """
 
+        key1 = "pass"
+        key2 = "word"
         self.name = "Mongo_Server"
         self.user = "mongo_user"
         self.japwd = "mongo_pwd"
@@ -63,6 +69,71 @@ class UnitTest(unittest.TestCase):
         self.port = 27017
         self.dbs = "test"
         self.db_auth = None
+        self.use_uri = True
+        self.use_arg = True
+        self.auth_db = "sysmon"
+        self.config = {key1 + key2: self.japwd}
+        self.conn_list = [self.host + ":" + str(self.port)]
+
+    def test_conn_list_attr(self):
+
+        """Function:  test_conn_list_attr
+
+        Description:  Test setting the conn_list attribute.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.DB(self.name, self.user, self.japwd,
+                               self.host, self.port)
+
+        self.assertEqual(mongo.conn_list, self.conn_list)
+
+    def test_config_attr(self):
+
+        """Function:  test_config_attr
+
+        Description:  Test setting the config attribute.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.DB(self.name, self.user, self.japwd,
+                               self.host, self.port)
+
+        self.assertEqual(mongo.config, self.config)
+
+    def test_using_auth_db(self):
+
+        """Function:  test_using_auth_db
+
+        Description:  Test using the auth_db attribute.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.DB(self.name, self.user, self.japwd,
+                               self.host, self.port, auth_db=self.auth_db)
+
+        self.assertEqual(mongo.auth_db, self.auth_db)
+
+    def test_using_arg(self):
+
+        """Function:  test_using_arg
+
+        Description:  Test with auth and arg present.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.DB(self.name, self.user, self.japwd,
+                               self.host, self.port, use_arg=self.use_arg)
+
+        self.assertTrue(mongo.use_arg)
 
     def test_auth_uri(self):
 
@@ -75,11 +146,9 @@ class UnitTest(unittest.TestCase):
         """
 
         mongo = mongo_class.DB(self.name, self.user, self.japwd,
-                               self.host, self.port)
+                               self.host, self.port, use_uri=self.use_uri)
 
-        self.assertEqual(
-            (mongo.name, mongo.user, mongo.japwd, mongo.host, mongo.port),
-            (self.name, self.user, self.japwd, self.host, self.port))
+        self.assertTrue(mongo.use_uri)
 
     def test_no_auth(self):
 
