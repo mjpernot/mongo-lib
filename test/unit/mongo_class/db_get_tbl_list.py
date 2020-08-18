1be571d23a664d2ba6ac92837ad3451dcd81b06b
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-08-18 14:50:37 +0100

    Updated test/unit/mongo_class/db_get_tbl_list.py v4.0.0

--- test/unit/mongo_class/db_get_tbl_list.py
+++ test/unit/mongo_class/db_get_tbl_list.py
@@ -98,7 +98,7 @@ class UnitTest(unittest.TestCase):
 
         self.name = "Mongo_Server"
         self.user = "mongo_user"
-        self.passwd = "mongo_pwd"
+        self.japwd = "mongo_pwd"
         self.host = "host_server"
         self.port = 27017
         self.dbs = "test"
@@ -114,7 +114,7 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        mongo = mongo_class.DB(self.name, self.user, self.passwd,
+        mongo = mongo_class.DB(self.name, self.user, self.japwd,
                                self.host, self.port)
         mongo.db = DBValidate()
 
