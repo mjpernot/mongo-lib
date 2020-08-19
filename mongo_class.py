414e585597d51669b7c4c84f2276e6ea283d4ee3
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-08-19 12:52:32 +0100

    Updated mongo_class.MasterRep.__init__ v4.0.0 - Passed new attributes to super command for the Rep class

--- mongo_class.py
+++ mongo_class.py
@@ -817,7 +817,7 @@ class MasterRep(Rep):
 
     """
 
-    def __init__(self, name, user, passwd, host="localhost", port=27017,
+    def __init__(self, name, user, japwd, host="localhost", port=27017,
                  **kwargs):
 
         """Method:  __init__
@@ -827,19 +827,25 @@ class MasterRep(Rep):
         Arguments:
             (input) name -> Name of server.
             (input) user -> User's name.
-            (input) passwd -> User's password.
+            (input) japwd -> User's psword.
             (input) host -> 'localhost' or host name or IP.
             (input) port -> '27017' or port for Mongo.
             (input) kwargs:
                 auth -> True|False - Authenication on.
                 conf_file -> Location of mongo.conf file.
+                use_uri -> True|False - Use uri to conenct to Mongo.
+                use_arg -> True|False - Use arguments to connect to Mongo.
+                auth_db -> Authenciation database name.
 
         """
 
         super(MasterRep, self).__init__(
-            name, user, passwd, host=host, port=port,
+            name, user, japwd, host=host, port=port,
             auth=kwargs.get("auth", True),
-            conf_file=kwargs.get("conf_file", None))
+            conf_file=kwargs.get("conf_file", None),
+            use_uri=kwargs.get("use_uri", False),
+            use_arg=kwargs.get("use_arg", False),
+            auth_db=kwargs.get("auth_db", "admin"))
 
         self.ismaster = None
         self.issecondary = None
