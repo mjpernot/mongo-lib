bdcf75f588f128054cf385e88ea7d7473ed25db2
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-08-19 11:47:12 +0100

    Updated test/unit/mongo_class/repset_init.py v4.0.0

--- test/unit/mongo_class/repset_init.py
+++ test/unit/mongo_class/repset_init.py
@@ -57,7 +57,7 @@ class UnitTest(unittest.TestCase):
 
         self.name = "Mongo_Server"
         self.user = "mongo_user"
-        self.passwd = "mongo_pwd"
+        self.japwd = "mongo_pwd"
         self.host = "host_server"
         self.port = 27017
         self.dbs = "test"
@@ -75,13 +75,14 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        mongo = mongo_class.RepSet(self.name, self.user, self.passwd,
+        mongo = mongo_class.RepSet(self.name, self.user, self.japwd,
                                    self.host, self.port, repset=self.repset)
 
-        self.assertEqual((mongo.name, mongo.user, mongo.passwd, mongo.host,
-                          mongo.port, mongo.repset),
-                         (self.name, self.user, self.passwd, self.host,
-                          self.port, self.repset))
+        self.assertEqual(
+            (mongo.name, mongo.user, mongo.japwd, mongo.host, mongo.port,
+             mongo.repset),
+            (self.name, self.user, self.japwd, self.host, self.port,
+             self.repset))
 
 
 if __name__ == "__main__":
