2c492bf5aa76e4f301b8c8e8d6fcb3ecea030bdd
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-08-21 09:10:40 +0100

    Updated test/unit/mongo_libs/crt_coll_inst.py v4.0.0

--- test/unit/mongo_libs/crt_coll_inst.py
+++ test/unit/mongo_libs/crt_coll_inst.py
@@ -57,7 +57,7 @@ class Cfg(object):
 
         self.name = "name"
         self.user = "user"
-        self.passwd = "passwd"
+        self.japwd = "pswd"
         self.host = "host"
         self.port = 27017
         self.auth = True
@@ -65,6 +65,42 @@ class Cfg(object):
         self.conf_file = "conf_file"
         self.repset_hosts = repset_hosts
         self.db_auth = "db_name"
+
+
+class Cfg2(object):
+
+    """Class:  Cfg2
+
+    Description:  Class stub holder for Cfg class with new attributes.
+
+    Methods:
+        __init__ -> Class initialization.
+
+    """
+
+    def __init__(self, repset_hosts=None):
+
+        """Function:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+
+        """
+
+        self.name = "name"
+        self.user = "user"
+        self.japwd = "pswd"
+        self.host = "host"
+        self.port = 27017
+        self.auth = True
+        self.repset = "repset_name"
+        self.conf_file = "conf_file"
+        self.repset_hosts = repset_hosts
+        self.db_auth = "db_name"
+        self.use_uri = False
+        self.use_arg = True
+        self.auth_db = "mydatabase"
 
 
 class UnitTest(unittest.TestCase):
@@ -75,6 +111,11 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_coll_new_attrs2 -> Test new use_uri, use_arg, & auth_db attrs.
+        test_repsetcoll_new_attrs2 -> Test new use_uri, use_arg, auth_db attr.
+        test_coll_new_attrs -> Test with new use_uri, use_arg, & auth_db attrs.
+        test_repsetcoll_new_attrs -> Test new use_uri, use_arg, & auth_db attr.
+        test_coll -> Test the mongo_class.Coll class.
         test_repsetcoll -> Test the mongo_class.RepSetColl class.
 
     """
@@ -90,6 +131,70 @@ class UnitTest(unittest.TestCase):
         """
 
         self.repset_hosts = "host:port"
+
+    @mock.patch("mongo_libs.mongo_class.Coll")
+    def test_coll_new_attrs2(self, mock_mongo):
+
+        """Function:  test_coll_new_attrs2
+
+        Description:  Test with new use_uri, use_arg, and auth_db attrs.
+
+        Arguments:
+
+        """
+
+        cfg = Cfg2()
+        mock_mongo.return_value = True
+
+        self.assertTrue(mongo_libs.crt_coll_inst(cfg, "db", "tbl"))
+
+    @mock.patch("mongo_libs.mongo_class.RepSetColl")
+    def test_repsetcoll_new_attrs2(self, mock_mongo):
+
+        """Function:  test_repsetcoll_new_attrs2
+
+        Description:  Test with new use_uri, use_arg, and auth_db attrs.
+
+        Arguments:
+
+        """
+
+        cfg = Cfg2(self.repset_hosts)
+        mock_mongo.return_value = True
+
+        self.assertTrue(mongo_libs.crt_coll_inst(cfg, "db", "tbl"))
+
+    @mock.patch("mongo_libs.mongo_class.Coll")
+    def test_coll_new_attrs(self, mock_mongo):
+
+        """Function:  test_coll_new_attrs
+
+        Description:  Test with new use_uri, use_arg, and auth_db attrs.
+
+        Arguments:
+
+        """
+
+        cfg = Cfg()
+        mock_mongo.return_value = True
+
+        self.assertTrue(mongo_libs.crt_coll_inst(cfg, "db", "tbl"))
+
+    @mock.patch("mongo_libs.mongo_class.RepSetColl")
+    def test_repsetcoll_new_attrs(self, mock_mongo):
+
+        """Function:  test_repsetcoll_new_attrs
+
+        Description:  Test with new use_uri, use_arg, and auth_db attrs.
+
+        Arguments:
+
+        """
+
+        cfg = Cfg(self.repset_hosts)
+        mock_mongo.return_value = True
+
+        self.assertTrue(mongo_libs.crt_coll_inst(cfg, "db", "tbl"))
 
     @mock.patch("mongo_libs.mongo_class.Coll")
     def test_coll(self, mock_mongo):
