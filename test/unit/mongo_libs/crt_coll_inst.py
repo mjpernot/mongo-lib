80d71b02db0f329f443997ff485ad19be756458f
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-08-21 13:58:03 +0100

    Updated test/unit/mongo_libs/crt_coll_inst.py v4.0.0

--- test/unit/mongo_libs/crt_coll_inst.py
+++ test/unit/mongo_libs/crt_coll_inst.py
@@ -126,7 +126,7 @@ class CfgA(object):
 
         self.name = "name"
         self.user = "user"
-        self.japwd = "usrpd"
+        self.japd = "usrpd"
         self.host = "host"
         self.port = 27017
         self.auth = True
