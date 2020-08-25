3128ffba33daed7622d4df4a6dea3ba6e67600fd
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-08-25 10:17:10 +0100

    Updated unit test run files v4.0.0

--- test/unit/mongo_class/unit_test_run.sh
+++ test/unit/mongo_class/unit_test_run.sh
@@ -27,6 +27,7 @@ test/unit/mongo_class/masterrep_connect.py
 test/unit/mongo_class/masterrep_init.py
 test/unit/mongo_class/repsetcoll_coll_cnt.py
 test/unit/mongo_class/repsetcoll_coll_del_many.py
+test/unit/mongo_class/repsetcoll_coll_dst.py
 test/unit/mongo_class/repsetcoll_coll_find.py
 test/unit/mongo_class/repsetcoll_connect.py
 test/unit/mongo_class/repsetcoll_db_auth.py
