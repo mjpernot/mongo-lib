03dd9daf3a72853b8507a1f0925453288c9d5315
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-03 09:59:20 +0100

    Updated test/integration/mongo_class/code_coverage.sh v4.0.0

--- test/integration/mongo_class/code_coverage.sh
+++ test/integration/mongo_class/code_coverage.sh
@@ -22,7 +22,7 @@ echo "Running unit test modules in conjunction with coverage"
 #coverage run -a --source=mongo_class test/integration/mongo_class/db_chg_db.py
 coverage run -a --source=mongo_class test/integration/mongo_class/db_connect.py
 #coverage run -a --source=mongo_class test/integration/mongo_class/db_db_cmd.py
-#coverage run -a --source=mongo_class test/integration/mongo_class/db_db_connect.py
+coverage run -a --source=mongo_class test/integration/mongo_class/db_db_connect.py
 #coverage run -a --source=mongo_class test/integration/mongo_class/db_get_tbl_list.py
 coverage run -a --source=mongo_class test/integration/mongo_class/db_init.py
 #coverage run -a --source=mongo_class test/integration/mongo_class/db_validate_tbl.py
