579132c572bd25a3d616fddf426d6968fee7d6d5
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-04 10:13:54 +0100

    Updated test/integration/mongo_class/integration_test_run.sh v4.0.0

--- test/integration/mongo_class/integration_test_run.sh
+++ test/integration/mongo_class/integration_test_run.sh
@@ -15,7 +15,6 @@ test/integration/mongo_class/coll_coll_find1.py
 #test/integration/mongo_class/coll_coll_options.py
 test/integration/mongo_class/coll_connect.py
 test/integration/mongo_class/coll_init.py
-#test/integration/mongo_class/coll_ins_doc.py
 test/integration/mongo_class/db_chg_db.py
 test/integration/mongo_class/db_connect.py
 test/integration/mongo_class/db_db_cmd.py
