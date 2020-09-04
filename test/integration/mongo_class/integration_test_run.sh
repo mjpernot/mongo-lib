3b2a0a4d21d6c369ede16c073cb7b362d2ff5a65
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-04 11:48:09 +0100

    Updated test/integration/mongo_class/integration_test_run.sh v4.0.0

--- test/integration/mongo_class/integration_test_run.sh
+++ test/integration/mongo_class/integration_test_run.sh
@@ -22,7 +22,7 @@ test/integration/mongo_class/db_db_connect.py
 test/integration/mongo_class/db_get_tbl_list.py
 test/integration/mongo_class/db_init.py
 test/integration/mongo_class/db_validate_tbl.py
-#test/integration/mongo_class/masterrep_connect.py
+test/integration/mongo_class/masterrep_connect.py
 test/integration/mongo_class/masterrep_init.py
 #test/integration/mongo_class/repsetcoll_coll_cnt.py
 #test/integration/mongo_class/repsetcoll_coll_del_many.py
