6e562ea567a5d6432f7e03025babfc5f3b4c126d
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-04 10:12:58 +0100

    Updated test/integration/mongo_class/integration_test_run.sh v4.0.0

--- test/integration/mongo_class/integration_test_run.sh
+++ test/integration/mongo_class/integration_test_run.sh
@@ -11,7 +11,7 @@ echo "Unit test: mongo_class"
 test/integration/mongo_class/coll_coll_cnt.py
 test/integration/mongo_class/coll_coll_dst.py
 test/integration/mongo_class/coll_coll_find.py
-#test/integration/mongo_class/coll_coll_find1.py
+test/integration/mongo_class/coll_coll_find1.py
 #test/integration/mongo_class/coll_coll_options.py
 test/integration/mongo_class/coll_connect.py
 test/integration/mongo_class/coll_init.py
