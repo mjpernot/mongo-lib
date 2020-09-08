4e9f5f6f5e90292283291e44c05220bb69c678b7
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-08 17:17:09 +0100

    Updated test/unit/mongo_class/slaverep_connect.py v4.0.0

--- test/unit/mongo_class/slaverep_connect.py
+++ test/unit/mongo_class/slaverep_connect.py
@@ -42,8 +42,21 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_primary_attr2 -> Test primary attribute.
+        test_primary_attr -> Test primary attribute.
+        test_repset_attr2 -> Test repset attribute.
+        test_repset_attr -> Test repset attribute.
+        test_issecondary_attr2 -> Test issecondary attribute.
+        test_issecondary_attr -> Test issecondary attribute.
+        test_ismaster_attr2 -> Test ismaster attribute.
+        test_ismaster_attr -> Test ismaster attribute.
+        test_no_conn_list1 -> Test with no connections passed.
+        test_no_conn_list -> Test with no connections passed.
+        test_fail_connection2 -> Test with failed connection.
         test_fail_connection -> Test with failed connection.
+        test_no_data2 -> Test with no data returned.
         test_no_data -> Test with no data returned.
+        test_default2 -> Test with minimum number of arguments.
         test_default -> Test with minimum number of arguments.
 
     """
@@ -71,6 +84,207 @@ class UnitTest(unittest.TestCase):
                      "issecondary": True, "setName": "mongo_repset",
                      "primary": "primary_host"}
         self.msg = "Error:  This is not a Slave Replication server."
+
+    @mock.patch("mongo_class.Server.connect",
+                mock.Mock(return_value=(True, None)))
+    @mock.patch("mongo_class.fetch_ismaster")
+    def test_primary_attr2(self, mock_fetch):
+
+        """Function:  test_primary_attr2
+
+        Description:  Test primary attribute.
+
+        Arguments:
+
+        """
+
+        mock_fetch.return_value = self.data
+        mongo = mongo_class.SlaveRep(self.name, self.user, self.japd,
+                                     self.host, self.port)
+        mongo.connect()
+
+        self.assertEqual(mongo.primary, "primary_host")
+
+    @mock.patch("mongo_class.Server.connect",
+                mock.Mock(return_value=(True, None)))
+    @mock.patch("mongo_class.fetch_ismaster")
+    def test_primary_attr(self, mock_fetch):
+
+        """Function:  test_primary_attr
+
+        Description:  Test primary attribute.
+
+        Arguments:
+
+        """
+
+        mock_fetch.return_value = self.data
+        mongo = mongo_class.SlaveRep(self.name, self.user, self.japd,
+                                     self.host, self.port)
+
+        self.assertEqual(mongo.connect(), (True, None))
+
+    @mock.patch("mongo_class.Server.connect",
+                mock.Mock(return_value=(True, None)))
+    @mock.patch("mongo_class.fetch_ismaster")
+    def test_repset_attr2(self, mock_fetch):
+
+        """Function:  test_repset_attr2
+
+        Description:  Test repset attribute.
+
+        Arguments:
+
+        """
+
+        mock_fetch.return_value = self.data
+        mongo = mongo_class.SlaveRep(self.name, self.user, self.japd,
+                                     self.host, self.port)
+        mongo.connect()
+
+        self.assertEqual(mongo.repset, "mongo_repset")
+
+    @mock.patch("mongo_class.Server.connect",
+                mock.Mock(return_value=(True, None)))
+    @mock.patch("mongo_class.fetch_ismaster")
+    def test_repset_attr(self, mock_fetch):
+
+        """Function:  test_repset_attr
+
+        Description:  Test repset attribute.
+
+        Arguments:
+
+        """
+
+        mock_fetch.return_value = self.data
+        mongo = mongo_class.SlaveRep(self.name, self.user, self.japd,
+                                     self.host, self.port)
+
+        self.assertEqual(mongo.connect(), (True, None))
+
+    @mock.patch("mongo_class.Server.connect",
+                mock.Mock(return_value=(True, None)))
+    @mock.patch("mongo_class.fetch_ismaster")
+    def test_issecondary_attr2(self, mock_fetch):
+
+        """Function:  test_issecondary_attr2
+
+        Description:  Test issecondary attribute.
+
+        Arguments:
+
+        """
+
+        mock_fetch.return_value = self.data
+        mongo = mongo_class.SlaveRep(self.name, self.user, self.japd,
+                                     self.host, self.port)
+        mongo.connect()
+
+        self.assertTrue(mongo.issecondary)
+
+    @mock.patch("mongo_class.Server.connect",
+                mock.Mock(return_value=(True, None)))
+    @mock.patch("mongo_class.fetch_ismaster")
+    def test_issecondary_attr(self, mock_fetch):
+
+        """Function:  test_issecondary_attr
+
+        Description:  Test issecondary attribute.
+
+        Arguments:
+
+        """
+
+        mock_fetch.return_value = self.data
+        mongo = mongo_class.SlaveRep(self.name, self.user, self.japd,
+                                     self.host, self.port)
+
+        self.assertEqual(mongo.connect(), (True, None))
+
+    @mock.patch("mongo_class.Server.connect",
+                mock.Mock(return_value=(True, None)))
+    @mock.patch("mongo_class.fetch_ismaster")
+    def test_ismaster_attr2(self, mock_fetch):
+
+        """Function:  test_ismaster_attr2
+
+        Description:  Test ismaster attribute.
+
+        Arguments:
+
+        """
+
+        mock_fetch.return_value = self.data
+        mongo = mongo_class.SlaveRep(self.name, self.user, self.japd,
+                                     self.host, self.port)
+        mongo.connect()
+
+        self.assertFalse(mongo.ismaster)
+
+    @mock.patch("mongo_class.Server.connect",
+                mock.Mock(return_value=(True, None)))
+    @mock.patch("mongo_class.fetch_ismaster")
+    def test_ismaster_attr(self, mock_fetch):
+
+        """Function:  test_ismaster_attr
+
+        Description:  Test ismaster attribute.
+
+        Arguments:
+
+        """
+
+        mock_fetch.return_value = self.data
+        mongo = mongo_class.SlaveRep(self.name, self.user, self.japd,
+                                     self.host, self.port)
+
+        self.assertEqual(mongo.connect(), (True, None))
+
+    @mock.patch("mongo_class.Server.connect",
+                mock.Mock(return_value=(True, None)))
+    @mock.patch("mongo_class.fetch_ismaster")
+    def test_no_conn_list1(self, mock_fetch):
+
+        """Function:  test_no_conn_list1
+
+        Description:  Test with no connections passed.
+
+        Arguments:
+
+        """
+
+        mock_fetch.return_value = self.data
+        mongo = mongo_class.SlaveRep(self.name, self.user, self.japd,
+                                     self.host, self.port)
+        mongo.conn = True
+        mongo.connect()
+
+        self.assertEqual(
+            (mongo.name, mongo.user, mongo.japd, mongo.host, mongo.port,
+             mongo.ismaster, mongo.issecondary),
+            (self.name, self.user, self.japd, self.host, self.port, False,
+             True))
+
+    @mock.patch("mongo_class.Server.connect",
+                mock.Mock(return_value=(True, None)))
+    @mock.patch("mongo_class.fetch_ismaster")
+    def test_no_conn_list(self, mock_fetch):
+
+        """Function:  test_no_conn_list
+
+        Description:  Test with no connections passed.
+
+        Arguments:
+
+        """
+
+        mock_fetch.return_value = self.data
+        mongo = mongo_class.SlaveRep(self.name, self.user, self.japd,
+                                     self.host, self.port)
+        mongo.conn = True
+
+        self.assertEqual(mongo.connect(), (True, None))
 
     @mock.patch("mongo_class.Server.connect",
                 mock.Mock(return_value=(False, "Error Message")))
