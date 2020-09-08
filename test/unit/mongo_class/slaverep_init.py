9fbced72c992e42b5460d3a3d34d44742c083fb9
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-08 17:17:05 +0100

    Updated test/unit/mongo_class/slaverep_init.py v4.0.0

--- test/unit/mongo_class/slaverep_init.py
+++ test/unit/mongo_class/slaverep_init.py
@@ -41,11 +41,22 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_primary_attr -> Test primary attribute.
+        test_repset_attr -> Test repset attribute.
+        test_issecondary_attr -> Test issecondary attribute.
+        test_ismaster_attr -> Test ismaster attribute.
+        test_no_conf_file_attr -> Test no conf_file attribute passed.
+        test_conf_file_attr -> Test conf_file attribute passed.
         test_conn_list_attr -> Test setting the conn_list attribute.
         test_config_attr -> Test setting the config attribute.
+        test_using_no_auth_db -> Test using no auth_db attribute.
         test_using_auth_db -> Test using the auth_db attribute.
+        test_no_using_arg -> Test with auth and no arg present.
         test_using_arg -> Test with auth and arg present.
+        test_no_auth_uri -> Test with auth and no uri present.
         test_auth_uri -> Test with auth and uri present.
+        test_auth_false -> Test with auth passed as False.
+        test_auth_true -> Test with auth passed as True.
         test_no_auth -> Test with no auth present.
 
     """
@@ -76,6 +87,103 @@ class UnitTest(unittest.TestCase):
         self.auth_db = "sysmon"
         self.config = {key1 + key2: self.japd}
         self.conn_list = [self.host + ":" + str(self.port)]
+        self.conf_file = "Config File"
+
+    def test_primary_attr(self):
+
+        """Function:  test_primary_attr
+
+        Description:  Test primary attribute.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.SlaveRep(
+            self.name, self.user, self.japd, self.host, self.port,
+            auth_db=self.auth_db)
+
+        self.assertFalse(mongo.primary)
+
+    def test_repset_attr(self):
+
+        """Function:  test_repset_attr
+
+        Description:  Test repset attribute.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.SlaveRep(
+            self.name, self.user, self.japd, self.host, self.port,
+            auth_db=self.auth_db)
+
+        self.assertFalse(mongo.repset)
+
+    def test_issecondary_attr(self):
+
+        """Function:  test_issecondary_attr
+
+        Description:  Test issecondary attribute.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.SlaveRep(
+            self.name, self.user, self.japd, self.host, self.port,
+            auth_db=self.auth_db)
+
+        self.assertFalse(mongo.issecondary)
+
+    def test_ismaster_attr(self):
+
+        """Function:  test_ismaster_attr
+
+        Description:  Test ismaster attribute.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.SlaveRep(
+            self.name, self.user, self.japd, self.host, self.port,
+            auth_db=self.auth_db)
+
+        self.assertFalse(mongo.ismaster)
+
+    def test_no_conf_file_attr(self):
+
+        """Function:  test_no_conf_file_attr
+
+        Description:  Test no conf_file attribute passed.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.SlaveRep(
+            self.name, self.user, self.japd, self.host, self.port,
+            auth_db=self.auth_db)
+
+        self.assertFalse(mongo.conf_file)
+
+    def test_conf_file_attr(self):
+
+        """Function:  test_conf_file_attr
+
+        Description:  Test conf_file attribute passed.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.SlaveRep(
+            self.name, self.user, self.japd, self.host, self.port,
+            auth_db=self.auth_db, conf_file=self.conf_file)
+
+        self.assertEqual(mongo.conf_file, self.conf_file)
 
     def test_conn_list_attr(self):
 
@@ -109,6 +217,21 @@ class UnitTest(unittest.TestCase):
 
         self.assertEqual(mongo.config, self.config)
 
+    def test_using_no_auth_db(self):
+
+        """Function:  test_using_no_auth_db
+
+        Description:  Test using no auth_db attribute.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.SlaveRep(
+            self.name, self.user, self.japd, self.host, self.port)
+
+        self.assertEqual(mongo.auth_db, "admin")
+
     def test_using_auth_db(self):
 
         """Function:  test_using_auth_db
@@ -124,6 +247,21 @@ class UnitTest(unittest.TestCase):
             auth_db=self.auth_db)
 
         self.assertEqual(mongo.auth_db, self.auth_db)
+
+    def test_no_using_arg(self):
+
+        """Function:  test_no_using_arg
+
+        Description:  Test with auth and no arg present.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.SlaveRep(
+            self.name, self.user, self.japd, self.host, self.port)
+
+        self.assertFalse(mongo.use_arg)
 
     def test_using_arg(self):
 
@@ -141,6 +279,21 @@ class UnitTest(unittest.TestCase):
 
         self.assertTrue(mongo.use_arg)
 
+    def test_no_auth_uri(self):
+
+        """Function:  test_no_auth_uri
+
+        Description:  Test with auth and no uri present.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.SlaveRep(
+            self.name, self.user, self.japd, self.host, self.port)
+
+        self.assertFalse(mongo.use_uri)
+
     def test_auth_uri(self):
 
         """Function:  test_auth_uri
@@ -156,6 +309,44 @@ class UnitTest(unittest.TestCase):
             use_uri=self.use_uri)
 
         self.assertTrue(mongo.use_uri)
+
+    def test_auth_false(self):
+
+        """Function:  test_auth_false
+
+        Description:  Test with auth passed as False.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.SlaveRep(self.name, self.user, self.japd,
+                                     self.host, self.port, auth=False)
+
+        self.assertEqual(
+            (mongo.name, mongo.user, mongo.japd, mongo.host, mongo.port,
+             mongo.ismaster, mongo.issecondary, mongo.auth),
+            (self.name, self.user, self.japd, self.host, self.port, None,
+             None, False))
+
+    def test_auth_true(self):
+
+        """Function:  test_auth_true
+
+        Description:  Test with auth passed as True.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.SlaveRep(self.name, self.user, self.japd,
+                                     self.host, self.port, auth=True)
+
+        self.assertEqual(
+            (mongo.name, mongo.user, mongo.japd, mongo.host, mongo.port,
+             mongo.ismaster, mongo.issecondary, mongo.auth),
+            (self.name, self.user, self.japd, self.host, self.port, None,
+             None, True))
 
     def test_no_auth(self):
 
