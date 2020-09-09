520fc44fb0d0f3170ce4aed20396cdced95aa792
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-09 12:45:00 +0100

    Updated test/integration/mongo_class/server_connect.py v4.0.0

--- test/integration/mongo_class/server_connect.py
+++ test/integration/mongo_class/server_connect.py
@@ -42,9 +42,16 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_conn_true2 -> Test with conn set to true.
+        test_conn_true -> Test with conn set to true.
+        test_fail_get_srv_attr2 -> Test with failed get_srv_attr call.
         test_fail_get_srv_attr -> Test with failed get_srv_attr call.
+        test_auth_arg4 -> Test with arg present and no auth.
+        test_auth_arg3 -> Test with arg present and no auth.
         test_auth_arg2 -> Test with auth and arg present.
         test_auth_arg -> Test with auth and arg present.
+        test_auth_uri4 -> Test with uri present and no auth.
+        test_auth_uri3 -> Test with uri present and no auth.
         test_auth_uri2 -> Test with auth and uri present.
         test_auth_uri -> Test with auth and uri present.
         test_no_auth2 -> Test with no auth present.
@@ -67,6 +74,75 @@ class UnitTest(unittest.TestCase):
         self.config_name = "mongo"
         self.cfg = gen_libs.load_module(self.config_name, self.config_dir)
 
+    def test_conn_true2(self):
+
+        """Function:  test_conn_true2
+
+        Description:  Test with conn set to True.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.Server(
+            self.cfg.name, self.cfg.user, self.cfg.japd, host=self.cfg.host,
+            port=self.cfg.port, use_uri=self.cfg.use_uri, auth=self.cfg.auth,
+            use_arg=self.cfg.use_arg, auth_db=self.cfg.auth_db,
+            conf_file=self.cfg.conf_file)
+        mongo.connect()
+
+        self.assertEqual(mongo.connect(), (True, None))
+
+    def test_conn_true(self):
+
+        """Function:  test_conn_true
+
+        Description:  Test with conn set to True.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.Server(
+            self.cfg.name, self.cfg.user, self.cfg.japd, host=self.cfg.host,
+            port=self.cfg.port, use_uri=self.cfg.use_uri, auth=self.cfg.auth,
+            use_arg=self.cfg.use_arg, auth_db=self.cfg.auth_db,
+            conf_file=self.cfg.conf_file)
+        mongo.connect()
+        mongo.connect()
+
+        self.assertEqual(
+            (mongo.name, mongo.user, mongo.japd, mongo.host, mongo.port,
+             mongo.use_arg),
+            (self.cfg.name, self.cfg.user, self.cfg.japd, self.cfg.host,
+             self.cfg.port, self.cfg.use_arg))
+
+    def test_fail_get_srv_attr2(self):
+
+        """Function:  test_fail_get_srv_attr2
+
+        Description:  Test with failed get_srv_attr call.
+
+        Arguments:
+
+        """
+
+        msg = "Authentication failed."
+        errmsg = "Error:  Auth flag or login params is incorrect: %s" % msg
+
+        mongo = mongo_class.Server(
+            self.cfg.name, self.cfg.user, "mytestpd", host=self.cfg.host,
+            port=self.cfg.port, use_uri=self.cfg.use_uri, auth=self.cfg.auth,
+            use_arg=self.cfg.use_arg, auth_db=self.cfg.auth_db,
+            conf_file=self.cfg.conf_file)
+        mongo.connect()
+
+        self.assertEqual(
+            (mongo.name, mongo.user, mongo.japd, mongo.host, mongo.port,
+             mongo.use_arg),
+            (self.cfg.name, self.cfg.user, "mytestpd", self.cfg.host,
+             self.cfg.port, self.cfg.use_arg))
+
     def test_fail_get_srv_attr(self):
 
         """Function:  test_fail_get_srv_attr
@@ -87,6 +163,47 @@ class UnitTest(unittest.TestCase):
             conf_file=self.cfg.conf_file)
 
         self.assertEqual(mongo.connect(), (False, errmsg))
+
+    def test_auth_arg4(self):
+
+        """Function:  test_auth_arg4
+
+        Description:  Test with arg present and no auth.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.Server(
+            self.cfg.name, self.cfg.user, self.cfg.japd, host=self.cfg.host,
+            port=self.cfg.port, use_uri=self.cfg.use_uri, auth=False,
+            use_arg=self.cfg.use_arg, auth_db=self.cfg.auth_db,
+            conf_file=self.cfg.conf_file)
+
+        self.assertEqual(mongo.connect(), (True, None))
+
+    def test_auth_arg3(self):
+
+        """Function:  test_auth_arg3
+
+        Description:  Test with arg present and no auth.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.Server(
+            self.cfg.name, self.cfg.user, self.cfg.japd, host=self.cfg.host,
+            port=self.cfg.port, use_uri=self.cfg.use_uri, auth=False,
+            use_arg=self.cfg.use_arg, auth_db=self.cfg.auth_db,
+            conf_file=self.cfg.conf_file)
+        mongo.connect()
+
+        self.assertEqual(
+            (mongo.name, mongo.user, mongo.japd, mongo.host, mongo.port,
+             mongo.use_arg),
+            (self.cfg.name, self.cfg.user, self.cfg.japd, self.cfg.host,
+             self.cfg.port, self.cfg.use_arg))
 
     def test_auth_arg2(self):
 
@@ -128,6 +245,45 @@ class UnitTest(unittest.TestCase):
              mongo.use_arg),
             (self.cfg.name, self.cfg.user, self.cfg.japd, self.cfg.host,
              self.cfg.port, self.cfg.use_arg))
+
+    def test_auth_uri4(self):
+
+        """Function:  test_auth_uri4
+
+        Description:  Test with uri present and no auth.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.Server(
+            self.cfg.name, self.cfg.user, self.cfg.japd, host=self.cfg.host,
+            port=self.cfg.port, use_uri=self.cfg.use_uri, auth=False,
+            use_arg=False, auth_db=True, conf_file=self.cfg.conf_file)
+        mongo.connect()
+
+        self.assertEqual(
+            (mongo.name, mongo.user, mongo.japd, mongo.host, mongo.port,
+             mongo.use_uri),
+            (self.cfg.name, self.cfg.user, self.cfg.japd, self.cfg.host,
+             self.cfg.port, self.cfg.use_uri))
+
+    def test_auth_uri3(self):
+
+        """Function:  test_auth_uri3
+
+        Description:  Test with uri present and no auth.
+
+        Arguments:
+
+        """
+
+        mongo = mongo_class.Server(
+            self.cfg.name, self.cfg.user, self.cfg.japd, host=self.cfg.host,
+            port=self.cfg.port, use_uri=self.cfg.use_uri, auth=False,
+            use_arg=False, auth_db=True, conf_file=self.cfg.conf_file)
+
+        self.assertEqual(mongo.connect(), (True, None))
 
     def test_auth_uri2(self):
 
