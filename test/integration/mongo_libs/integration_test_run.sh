1269e365824840233f252b6394849b40dd9e7c28
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-10 09:23:10 +0100

    Added test/integration/mongo_libs/integration_test_run.sh v4.0.0

---
+++ test/integration/mongo_libs/integration_test_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Integration testing program for the module.
+# This will run all the units tests for this program.
+# Will need to run this from the base directory where the module file 
+#   is located at.
+
+echo "Integration test: mongo_class"  
+test/integration/mongo_libs/create_cmd.py
+test/integration/mongo_libs/create_instance.py
+test/integration/mongo_libs/crt_base_cmd.py
+test/integration/mongo_libs/crt_coll_inst.py
+
