1ed09fe586de2b9cf979119a9167d1f8244d82c7
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-12-07 14:26:04 +0000

    Updated mongo_class.Rep.__init__ v4.1.0 - Added auth_mech argument to super class function call

--- mongo_class.py
+++ mongo_class.py
@@ -809,7 +809,8 @@ class Rep(Server):
                 conf_file -> Location of mongo.conf file.
                 use_uri -> True|False - Use uri to conenct to Mongo.
                 use_arg -> True|False - Use arguments to connect to Mongo.
-                auth_db -> Authenciation database name.
+                auth_db -> Authentication database name.
+                auth_mech -> Authentication mechanism for connecting.
 
         """
 
@@ -819,7 +820,8 @@ class Rep(Server):
             conf_file=kwargs.get("conf_file", None),
             use_uri=kwargs.get("use_uri", False),
             use_arg=kwargs.get("use_arg", False),
-            auth_db=kwargs.get("auth_db", "admin"))
+            auth_db=kwargs.get("auth_db", "admin"),
+            auth_mech = kwargs.get("auth_mech", "SCRAM-SHA-1"))
 
         self.repset = None
         self.ismaster = None
