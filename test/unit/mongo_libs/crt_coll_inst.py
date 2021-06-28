5a0ed3eaaf38e26c0abe0de10e5c570fcba3482d
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-06-28 13:08:23 +0100

    Updated test/unit/mongo_libs/crt_coll_inst.py v4.2.0

--- test/unit/mongo_libs/crt_coll_inst.py
+++ test/unit/mongo_libs/crt_coll_inst.py
@@ -102,6 +102,51 @@ class Cfg2(object):
         self.use_arg = True
         self.auth_db = "mydatabase"
         self.auth_mech = "SCRAM-SHA-1"
+        self.ssl_client_ca = None
+        self.ssl_client_cert = None
+        self.ssl_client_key = None
+        self.ssl_client_phrase = None
+
+
+class Cfg3(object):
+
+    """Class:  Cfg3
+
+    Description:  Class stub holder for Cfg class with new attributes.
+
+    Methods:
+        __init__ -> Class initialization.
+
+    """
+
+    def __init__(self, repset_hosts=None):
+
+        """Function:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+
+        """
+
+        self.name = "name"
+        self.user = "user"
+        self.japd = "userpd"
+        self.host = "host"
+        self.port = 27017
+        self.auth = True
+        self.repset = "repset_name"
+        self.conf_file = "conf_file"
+        self.repset_hosts = repset_hosts
+        self.db_auth = "db_name"
+        self.use_uri = False
+        self.use_arg = True
+        self.auth_db = "mydatabase"
+        self.auth_mech = "SCRAM-SHA-1"
+        self.ssl_client_ca = "CAFile"
+        self.ssl_client_cert = "CertFile"
+        self.ssl_client_key = "KeyFile"
+        self.ssl_client_phrase = "MyPhrase"
 
 
 class UnitTest(unittest.TestCase):
@@ -112,6 +157,12 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_coll_set_ssl -> Test mongo_class.Coll class with SSL set.
+        test_rep_set_ssl -> Test mongo_class.RepSetColl class with SSL set.
+        test_coll_none_ssl -> Test mongo_class.Coll class with SSL set to none.
+        test_rep_none_ssl -> Test RepSetColl class with SSL set to none.
+        test_coll_no_ssl -> Test the mongo_class.Coll class with no ssl.
+        test_rep_no_ssl -> Test the mongo_class.RepSetColl class with no ssl.
         test_coll_mech_auth -> Test with authentication mechanism passed.
         test_coll_no_mech_auth -> Test with no authentication mechanism passed.
         test_repset_mech_auth -> Test with authentication mechanism passed.
@@ -136,6 +187,96 @@ class UnitTest(unittest.TestCase):
         """
 
         self.repset_hosts = "host:port"
+
+    def test_coll_set_ssl(self):
+
+        """Function:  test_coll_set_ssl
+
+        Description:  Test mongo_class.Coll class with SSL set.
+
+        Arguments:
+
+        """
+
+        cfg = Cfg3()
+        mongo = mongo_libs.crt_coll_inst(cfg, "db", "tbl")
+
+        self.assertEqual(mongo.ssl_client_ca, cfg.ssl_client_ca)
+
+    def test_rep_set_ssl(self):
+
+        """Function:  test_rep_set_ssl
+
+        Description:  Test mongo_class.RepSetColl class with SSL set.
+
+        Arguments:
+
+        """
+
+        cfg = Cfg3(self.repset_hosts)
+        mongo = mongo_libs.crt_coll_inst(cfg, "db", "tbl")
+
+        self.assertEqual(mongo.ssl_client_ca, cfg.ssl_client_ca)
+
+    def test_coll_none_ssl(self):
+
+        """Function:  test_coll_none_ssl
+
+        Description:  Test mongo_class.Coll class with SSL set to none.
+
+        Arguments:
+
+        """
+
+        cfg = Cfg2()
+        mongo = mongo_libs.crt_coll_inst(cfg, "db", "tbl")
+
+        self.assertFalse(mongo.ssl_client_ca)
+
+    def test_rep_none_ssl(self):
+
+        """Function:  test_rep_none_ssl
+
+        Description:  Test mongo_class.RepSetColl class with SSL set to none.
+
+        Arguments:
+
+        """
+
+        cfg = Cfg2(self.repset_hosts)
+        mongo = mongo_libs.crt_coll_inst(cfg, "db", "tbl")
+
+        self.assertFalse(mongo.ssl_client_ca)
+
+    def test_coll_no_ssl(self):
+
+        """Function:  test_coll_no_ssl
+
+        Description:  Test the mongo_class.Coll class with no ssl.
+
+        Arguments:
+
+        """
+
+        cfg = Cfg()
+        mongo = mongo_libs.crt_coll_inst(cfg, "db", "tbl")
+
+        self.assertFalse(mongo.ssl_client_ca)
+
+    def test_rep_no_ssl(self):
+
+        """Function:  test_rep_no_ssl
+
+        Description:  Test the mongo_class.RepSetColl class with no ssl.
+
+        Arguments:
+
+        """
+
+        cfg = Cfg(self.repset_hosts)
+        mongo = mongo_libs.crt_coll_inst(cfg, "db", "tbl")
+
+        self.assertFalse(mongo.ssl_client_ca)
 
     @mock.patch("mongo_libs.mongo_class.RepSetColl")
     def test_coll_mech_auth(self, mock_mongo):
