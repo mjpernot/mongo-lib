303c0757eceefb4aee8f425ef684e34ebf9b1c4b
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2025-05-20 09:04:53 +0100

    mongo_class.Server methods updated to Pymongo 4.X v4.5.2

--- test/integration/mongo_class/rep_init.py
+++ test/integration/mongo_class/rep_init.py
@@ -96,41 +96,47 @@ class UnitTest(unittest.TestCase):
         self.config["password"] = self.cfg.japd
 
         self.config2 = {}
+        self.config["directConnection"] = False
+        self.config["authMechanism"] = self.auth_mech
         self.config2["password"] = self.cfg.japd
-        self.config2["authMechanism"] = self.auth_mech2
 
         self.config3 = {}
+        self.config["directConnection"] = False
+        self.config["authMechanism"] = self.auth_mech
         self.config3["password"] = self.cfg.japd
-        self.config3["authMechanism"] = self.auth_mech2
         self.config3["ssl"] = True
         self.config3["ssl_ca_certs"] = self.ssl_client_ca
 
         self.config4 = {}
+        self.config["directConnection"] = False
+        self.config["authMechanism"] = self.auth_mech
         self.config4["password"] = self.cfg.japd
-        self.config4["authMechanism"] = self.auth_mech2
         self.config4["ssl"] = True
         self.config4["ssl_keyfile"] = self.ssl_client_key
         self.config4["ssl_certfile"] = self.ssl_client_cert
 
         self.config5 = {}
+        self.config["directConnection"] = False
+        self.config["authMechanism"] = self.auth_mech
         self.config5["password"] = self.cfg.japd
-        self.config5["authMechanism"] = self.auth_mech2
         self.config5["ssl"] = True
         self.config5["ssl_keyfile"] = self.ssl_client_key
         self.config5["ssl_certfile"] = self.ssl_client_cert
         self.config5["ssl_pem_passphrase"] = self.ssl_client_phrase
 
         self.config6 = {}
+        self.config["directConnection"] = False
+        self.config["authMechanism"] = self.auth_mech
         self.config6["password"] = self.cfg.japd
-        self.config6["authMechanism"] = self.auth_mech2
         self.config6["ssl"] = True
         self.config6["ssl_ca_certs"] = self.ssl_client_ca
         self.config6["ssl_keyfile"] = self.ssl_client_key
         self.config6["ssl_certfile"] = self.ssl_client_cert
 
         self.config7 = {}
+        self.config["directConnection"] = False
+        self.config["authMechanism"] = self.auth_mech
         self.config7["password"] = self.cfg.japd
-        self.config7["authMechanism"] = self.auth_mech2
         self.config7["ssl"] = True
         self.config7["ssl_ca_certs"] = self.ssl_client_ca
         self.config7["ssl_keyfile"] = self.ssl_client_key
